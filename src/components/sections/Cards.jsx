2a82c9269061ad6e497a88974b967695f0636f85
Nitesh <niteshanthony3@gmail.com>
2026-02-03 11:30:01 -0500

    implemented the battle and linecards section

--- src/components/sections/Cards.jsx
+++ src/components/sections/Cards.jsx
@@ -5,7 +5,28 @@ const Cards = () => {
     <section id='cards' className='flex-center md:mt-40 mt-20 section-padding xl:px-0'>
         <div className="w-full h-full md:px-20 px-5">
             
-            <TitleHeader title="Professional Work Experience" />
+            <div className="flex items-center justify-center gap-2 md:gap-4">
+                <TitleHeader title="Why Choose" /> 
+                <TitleHeader title="RCA" className="text-orange-100"/>
+            </div>
+
+            <div className="mt-32 relative">
+                <div className="relative z-50 xl:space-y-32 space-y-10 flex flex-col items-center">
+
+                    <img 
+                    src="/images/navii.jpg" 
+                    alt="battlecard" 
+                    className="w-full max-w-4xl h-auto object-cover rounded-xl"
+                    />
+
+                    <img 
+                    src="/images/max.jpg" 
+                    alt="battlecard2" 
+                    className="w-full max-w-4xl h-auto object-cover rounded-xl"
+                    />
+
+                </div>
+            </div>
 
         </div>
     </section>
